c76512ce80e2c839fa8960ef68b112542e988194
Tohanean Adrian <tohaneanlucian@gmail.com>
2021-07-14 14:04:02 +0300

    Changed form tiltle from form 1 to Gamescreen

--- Xiangqi/Form1.Designer.cs
+++ Xiangqi/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Xiangqi
 {
-    partial class Form1
+    partial class GameScreen
     {
         /// <summary>
         ///  Required designer variable.
@@ -31,12 +31,12 @@ namespace Xiangqi
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // GameScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.Name = "GameScreen";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
--- Xiangqi/Form1.cs
+++ Xiangqi/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Xiangqi
 {
-    public partial class Form1 : Form
+    public partial class GameScreen : Form
     {
-        public Form1()
+        public GameScreen()
         {
             InitializeComponent();
         }
